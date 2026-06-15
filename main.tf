24fa29271fd664727ba2425687d9ffab68152d4f
Enrish-bit <carlenrishsantos@gmail.com>
2026-06-15 12:48:38 +0800

    feat: implement infrastructure as code using terraform

--- main.tf
+++ main.tf
@@ -1,33 +1,37 @@
+# 1. Tell Terraform we want to connect to Supabase
 terraform {
-    required_providers{
-        supabase = {
-            source = "supabase/supabase"
-            version = "~> 1.0"
-        }
+  required_providers {
+    supabase = {
+      source  = "supabase/supabase"
+      version = "~> 1.0"
     }
+  }
 }
 
 provider "supabase" {
-    api_key = var.supabase_api_key
+  access_token = var.supabase_access_token
 }
 
-variable "supabase_api_key" {
-    type        = string
-    description = "The access token for your Supabase account"
-    sensitive   = true
-}
 
-resource "supabase_project" "market_pipeline" {
-    organization_id = var.supabase_org_id
-    name    = "ph-market-pipeline-infra"
-    region  = "ap-southeast-1" 
-
-    database_config = {
-        password = var.db_password
-    }
+variable "supabase_access_token" {
+  type      = string
+  sensitive = true
 }
 
 variable "supabase_org_id" {
-    type        = string
-    sensitive   = true
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+
+resource "supabase_project" "market_pipeline" {
+  organization_id   = var.supabase_org_id
+  name              = "ph-market-pipeline-infra"
+  region            = "ap-southeast-1" # Singapore (closest to PH)
+  database_password = var.db_password
 }
